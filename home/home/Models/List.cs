dce1f0e39af784cb2335e2a3bdf5259e52a2d13c
булат шакиров <a2009-1@mail.ru>
2022-04-03 16:07:42 +0300

    add models

---
+++ home/home/Models/List.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace home.Models
+{
+    class List
+    {
+        public static List<Project> Projects = new List<Project>()
+        {
+
+        };
+    }
+}
